9d41607ba8f0ffa45e1399f33a2a6ee0006308d6
houweidong <377473270@qq.com>
2019-06-10 12:48:08 +0800

    add demo_yol

--- demo_yolo.py
+++ demo_yolo.py
@@ -1,3 +1,4 @@
+
 """YOLO Demo script."""
 import os
 import argparse
@@ -14,30 +15,82 @@ from mxnet import nd
 from utils.timer import Timer
 
 
-def forward(image, ctx, args):
-    if not args.demo:
-        x, img = presets.yolo.load_test(image, short=512)
+def w2f(image_d, ctx, args, img_bbox_d):
+    if os.path.isdir(image_d):
+
+        for image_dd in os.listdir(image_d):
+            dir = image_d.split('/')[-1]
+            try:
+                w2f(os.path.join(image_d, image_dd), ctx, args, os.path.join(img_bbox_d, dir))
+            except:
+                continue
+
     else:
-        x, img = presets.yolo.transform_test(image, short=512)
-    x = x.as_in_context(ctx[0])
-    ids, scores, bboxes = [xx[0].asnumpy() for xx in net(x)]
-    # select args.classes in ids
-    if args.classes:
-        cla_ids = []
-        # str.strip
-        clses = list(map(str.strip, filter(None, args.classes.split(','))))
-        cond_a = np.zeros_like(ids, dtype=np.bool)
-        for cls in clses:
-            cla_ids.append(COCODetection.CLASSES.index(cls))
+
+        # img_bbox_f = open(os.path.join(img_bbox_d, image_d.split('/')[-1] + '.txt'), 'a')
+        img_bbox_f = open(img_bbox_d + '.txt', 'a')
+        image = image_d
+        try:
+            ids, scores, bboxes, img = forward(image, ctx, args)
+        except:
+            raise Exception('valid picture')
+        else:
+            image_name = image.split('/')[-1]
+            bboxes_str = ''
+            for i, coord in enumerate(np.reshape(bboxes, -1)):
+                # if i != 0 and i % 4 == 0:
+                #     bboxes_str += ', '
+                bboxes_str += str(coord) + ' '
+            write2file = image_name + ' ' + bboxes_str + '\n'
+            img_bbox_f.write(write2file)
+            img_bbox_f.close()
+
+
+def forward(image_p, ctx, args):
+
+    try:
+        if not args.demo:
+            image = mx.image.imread(image_p)
+    except:
+        print(image_p)
+        os.remove(image_p)
+        raise Exception('valid picture')
+    else:
+        max_len = max(image.shape[0], image.shape[1])
+        min_len = min(image.shape[0], image.shape[1])
+        # short = int(args.short)
+        short = 320
+        x, _ = presets.yolo.transform_test(image, short=short, max_size=1024)
+        scale = min_len / short
+        if max_len / scale > 1024:
+            scale = max_len / 1024
+        img = image.asnumpy().astype('uint8')
+
+        x = x.as_in_context(ctx[0])
+        ids, scores, bboxes = [xx[0].asnumpy() for xx in net(x)]
+        bboxes = bboxes * scale
+        # select args.classes in ids
+        if args.classes:
+            cla_ids = []
+            # str.strip
+            clses = list(map(str.strip, filter(None, args.classes.split(','))))
+            cond_a = np.zeros_like(ids, dtype=np.bool)
+            for cls in clses:
+                cla_ids.append(COCODetection.CLASSES.index(cls))
             for cid in cla_ids:
                 cond = cid == ids.astype(np.int)
                 cond_a = cond | cond_a
-        # ids = np.where(cond_a, ids, -1)
-        # scores = np.where(cond_a, scores, -1)
-        # bboxes = np.where(cond_a, bboxes, -1)
+            # ids = np.where(cond_a, ids, -1)
+            # scores = np.where(cond_a, scores, -1)
+            # bboxes = np.where(cond_a, bboxes, -1)
+
+            cond_a = (scores >= args.thresh) & cond_a
+        else:
+            cond_a = scores >= args.thresh
         ids = ids[cond_a.reshape(-1)]
         scores = scores[cond_a.reshape(-1)]
         bboxes = bboxes[cond_a.reshape(-1)]
+
     # print(ids)
     # print(scores)
     # print(bboxes)
@@ -64,15 +117,15 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Test with YOLO networks.')
     parser.add_argument('--network', type=str, default='yolo3_darknet53_coco',
                         help="Base network name")
-    parser.add_argument('--images', type=str, default='',
+    parser.add_argument('--images', type=str, default='nanzhuang',
                         help='Test images, use comma to split multiple.')
     parser.add_argument('--gpus', type=str, default='0',
                         help='Training with GPUs, you can specify 1,3 for example.')
     parser.add_argument('--pretrained', type=str, default='',
                         help='Load weights from previously saved parameters.')
-    parser.add_argument('--thresh', type=float, default=0.05,
+    parser.add_argument('--thresh', type=float, default=0.2,
                         help='Threshold of object score when visualize the bboxes.')
-    parser.add_argument('--classes', type=str, default='',
+    parser.add_argument('--classes', type=str, default='person',
                         help='classes to be displayed. could more than one class')
     parser.add_argument('--coop-cfg', type=str, default='2, 2, 2',
                         help='coop configs. "," separate different output head, '
@@ -81,6 +134,8 @@ def parse_args():
     parser.add_argument('--nms-mode', type=str, default='Default', choices=['Default', 'Exclude', 'Merge'])
     parser.add_argument('--demo', action='store_true', help='whether to use camera or video.')
     parser.add_argument('--video', default=None, help='path to vedio')
+    parser.add_argument('--w2f', action='store_true', help='whether to dump to the file')
+    parser.add_argument('--root', type=str, default='/root/dataset/', help='dataset root')
     args = parser.parse_args()
     return args
 
@@ -95,7 +150,12 @@ if __name__ == '__main__':
     if not args.images.strip():
         val_data = '/root/dataset/coco2017/train2017'
         image_list = [os.path.join(val_data, img) for img in os.listdir(val_data)]
-
+    elif '.' not in args.images:
+        # for file in os.listdir(args.root):
+        #     print(file)
+        # print(os.listdir(args.root))
+        val_data = os.path.join(args.root, args.images)
+        image_list = [os.path.join(val_data, img) for img in os.listdir(val_data)]
     else:
         image_list = [os.path.join('./data', x.strip()) for x in args.images.split(',') if x.strip()]
 
@@ -107,7 +167,7 @@ if __name__ == '__main__':
         net.initialize()
         net.load_parameters(args.pretrained, allow_missing=True, ignore_extra=True)
         # print('wo')
-    net.set_nms(0.9, 200)
+    net.set_nms(0.5, 200)
     net.collect_params().reset_ctx(ctx=ctx)
 
     if args.demo:
@@ -139,9 +199,35 @@ if __name__ == '__main__':
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
     else:
+        if args.w2f:
+            # we write the file to the fixed dir /root/dataset/results
+            # img_bbox_f = open(os.path.join('/root/dataset/results', args.images+'.txt'),'w')
+            img_bbox_d = os.path.join('/root/dataset/results', args.images)
+
+            isExists = os.path.exists(img_bbox_d)
+
+            if not isExists:
+                os.makedirs(img_bbox_d)
+
         for image in image_list:
-            ax = None
-            ids, scores, bboxes, img = forward(image, ctx, args)
-            ax = gcv.utils.viz.plot_bbox(img, bboxes, scores, ids, thresh=args.thresh,
-                                         class_names=net.classes, ax=ax)
-            plt.show()
+
+            # ids, scores, bboxes, img = forward(image, ctx, args)
+            if args.w2f:
+                w2f(image, ctx, args, img_bbox_d)
+                # image_name = image.split('/')[-1]
+                # bboxes_str = ''
+                # for i, coord in enumerate(np.reshape(bboxes, -1)):
+                #     # if i != 0 and i % 4 == 0:
+                #     #     bboxes_str += ', '
+                #     bboxes_str += str(coord) + ' '
+                # write2file = image_name + ' ' + bboxes_str + '\n'
+                # img_bbox_f.write(write2file)
+            else:
+                ids, scores, bboxes, img = forward(image, ctx, args)
+                ax = None
+                ax = gcv.utils.viz.plot_bbox(img, bboxes, scores, ids, thresh=args.thresh,
+                                             class_names=net.classes, ax=ax)
+                plt.show()
+        # if args.w2f:
+        #     img_bbox_f.close()
+
